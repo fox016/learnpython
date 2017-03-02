193c40e3377a90d780c68e4bef150f4d0c17885e
Nate Fox <fox016@gmail.com>
2017-03-02 17:59:01 -0500

    challenge 3 (sudoku) done

--- sql/create.sql
+++ sql/create.sql
@@ -48,14 +48,3 @@ CREATE TABLE IF NOT EXISTS users
 	updated_date_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 	PRIMARY KEY(user_id)
 ) ENGINE=InnoDB;
-
-
-
-/* Initial challenge set data */
-INSERT INTO challenge_sets VALUES ('math', 'Math Challenges', 1, 'Math challenges include problems in algebra, geometry, trigonometry, number theory, sequences, prime numbers, etc. Test your math skills and your coding skills combined!', now(), now());
-INSERT INTO challenge_sets VALUES ('puzzle', 'Puzzle Games', 2, 'These puzzle games will test your problem solving skills and will require coding skills like iteration and recursion. Plus, the solutions will help you improve your puzzle game skills!', now(), now());
-
-/* Initial challenge data */
-INSERT INTO challenges VALUES ('cone_area', 'Surface Area of a Cone', 'math', 1, 1, 'A cone is a geometric shape. The base is a circle, and lines coming from every point on that circle converge at a single point.<p><img src="images/area_cone_pic.png"></p><p>The equation to calculate the surface area of a cone looks like this:</p><p><img src="images/area_cone_eq.png"></p><p>Write a function named <code>cone_area</code> that takes <code>height</code> and <code>radius</code> as parameters. Return the surface area of the cone.</p><p>Question: What is the surface area of a cone with a height of 2938 and radius of 761 (rounded to the nearest 2 decimal places)?</p><p>Hint: Using 3.14 for pi will not be precise enough. Use <code>math.pi</code> from Python&apos;s math module or use at least 10 digits of pi.</p>', '9075192.22', now(), now());
-INSERT INTO challenges VALUES ('fib1', 'Fibonacci Sequence', 'math', 2, 1, 'The Fibonacci sequence is a mathematical sequence. It starts with (1, 1, 2, 3, 5, 8, 13, ...). Notice that each value is the sum of the previous two values. Write a function named <code>fib</code> that takes <code>n</code>, an integer, as a parameter. Return the nth value of the fibonacci sequence. The mathematical definition of the sequence is<p>fib(n=0) = 1<br/>fib(n=1) = 1<br/>fib(n>1) = fib(n-1) + fib(n-2)</p><p>Question: What are the <strong>last 50 digits</strong> of <code>fib(20000)</code>?</p><p>Hint: Even though the mathematical definition uses recursion, you may want to consider using iteration to solve this problem.</p>', '18631347739229074282048749390382177080100224710626', now(), now());
-INSERT INTO challenges VALUES ('sudoku', 'Sudoku Solver', 'puzzle', 1, 2, 'TODO', 'TODO', now(), now());

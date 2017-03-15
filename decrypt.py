a6e8abde5436ae052ef7107065c2ff7da254262d
Nate Fox <fox016@gmail.com>
2017-03-15 19:29:21 -0400

    working on dec/enc challenge

---
+++ decrypt.py
@@ -0,0 +1,47 @@
+
+def encrypt(secret, key):
+	cipher = ""
+	for i in range(0, len(secret)):
+		x = ord(secret[i])
+		y = ord(key[i%len(key)])
+		if y % 4 == 0:
+			d = x + 3
+		if y % 4 == 1:
+			d = x + 4
+		if y % 4 == 2:
+			d = x + 5
+		if y % 4 == 3:
+			d = x + 6
+		cipher += chr(d)
+	return cipher
+
+def decrypt(cipher, key):
+	text = ""
+	for i in range(0, len(cipher)):
+		d = ord(cipher[i])
+		y = ord(key[i%len(key)])
+		if y % 4 == 0:
+			x = d - 3
+		if y % 4 == 1:
+			x = d - 4
+		if y % 4 == 2:
+			x = d - 5
+		if y % 4 == 3:
+			x = d - 6
+		text += chr(x)
+	return text
+
+secret = "John 1:1 - In the beginning was the Word, and the Word was with God, and the Word was God."
+key = "platypus"
+
+cipher = encrypt(secret, key)
+text = decrypt(cipher, key)
+
+# print decrypt("Mrlq$4>7#0$Lr#xnh#fhklrtlqk#{dw&wki#[rvj/#eqh#xnh#[rvg$}dv$zmwl&Jrh/$drj#wlh$Zsxg#{dw#Kug1", "platypus")
+
+for word in open("files/dictionary.txt"):
+	text = decrypt(cipher, word)
+	if word == "platypus":
+		print word, text
+	if text.find("In the beg") > 0:
+		print word, text

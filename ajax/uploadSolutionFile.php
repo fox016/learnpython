13cfda335f309b9044f794462fc580af477e75e7
Nate Fox <fox016@gmail.com>
2017-03-23 13:14:01 -0400

    converted decrypt key challenge to use uploaded file

--- ajax/uploadSolutionFile.php
+++ ajax/uploadSolutionFile.php
@@ -17,7 +17,7 @@ try
 	// Blacklist Python code
 	$blacklist = array(
 		"/mysql/i", "/natefox/i", "/subprocess/i", "/learnpython/i", "/popen/i", "/from commands/i", "/import commands/i", "/spawn/i", "/os.system/i",
-		"/open\(.*,.*\)/i", "/http/i"
+		"/open\(.*,.*\)/i", "/http/i", "/open\(^(?!.*(dictionary_file).*$)\)/i"
 	);
 	foreach($blacklist as $regex)
 	{
@@ -26,10 +26,13 @@ try
 			throw new Exception("Invalid Python: use of $regex");
 	}
 
+	// Replacements
+	$execPython = preg_replace("/dictionary_file.*('|\").*('|\")/i", "dictionary_file='/home/natefoxc/www/learnpython/files/dictionary.txt'", $userPython);
+
 	// Write Python file to tmp directory
 	$pyFile = "/tmp/{$userid}_{$filename}";
 	$outfile = "/tmp/{$userid}_{$filename}.txt";
-	file_put_contents($pyFile, $userPython, LOCK_EX);
+	file_put_contents($pyFile, $execPython, LOCK_EX);
 
 	// Add test code to file
 	$challengeId = $_POST['challengeId'];
@@ -37,7 +40,7 @@ try
 	file_put_contents($pyFile, $testCode, FILE_APPEND | LOCK_EX);
 
 	// Execute Python file (with timer)
-	$command = "timeout 30 python $pyFile &> $outfile";
+	$command = "timeout 45 python $pyFile &> $outfile";
 	system($command, $result);
 	$output = file_get_contents($outfile);
 	
@@ -53,7 +56,7 @@ try
 	for($i = 0; $i < count($testLines); $i++)
 	{
 		if(!isset($outputLines[$i]))
-			returnError($testLines[$i], $solutionLines[$i], NULL, "Not enough lines in output");
+			returnError($testLines[$i], $solutionLines[$i], NULL, "Not enough lines in output (line $i)");
 		if($outputLines[$i] !== $solutionLines[$i])	
 			returnError($testLines[$i], $solutionLines[$i], $outputLines[$i], "Incorrect value");
 	}
